832a765410cd70f109d927e83e3af5a3cf0fe488
Matheus Pereira <mtspereira.pro@gmail.com>
2026-05-07 14:29:08 -0300

    refactor: organize mock book data into feature structure

---
+++ app/src/features/books/data/mockBooks.js
@@ -0,0 +1,17 @@
+export const mockBooks = [
+  {
+    id: "1",
+    title: "Lord of Mysteries",
+    author: "Cuttlefish",
+  },
+  {
+    id: "2",
+    title: "Shadow Slave",
+    author: "Guiltythree",
+  },
+  {
+    id: "3",
+    title: "Reverend Insanity",
+    author: "Gu Zhen Ren",
+  },
+];
